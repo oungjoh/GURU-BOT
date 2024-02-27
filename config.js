24576517705071372d14ebcd5810d146611503f0
oungjoh <100134238+oungjoh@users.noreply.github.com>
2024-02-27 11:26:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['2349164976760', 'jogo bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
